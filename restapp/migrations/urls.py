ab236cc8d4a90770c204039cafb35547645d6a17
ahmed assad <aassad@itecorp.net>
2022-11-24 16:21:47 +0200

    small error

--- restapp/migrations/urls.py
+++ restapp/migrations/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from . import viewsurlpatterns =[
+from . import views
+urlpatterns =[
     path('guitars', views.guitars_handler),
     path('guitars/<int:guitar_id>', views.get_guitar_with_id),
 ]
